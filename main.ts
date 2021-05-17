74a4730f37e99ccd91e2089684c9e1e82cd61d49
Channing Hyde <73716702+channingolivia@users.noreply.github.com>
2021-05-17 10:58:27 -0600

    Simple hand washing timer using micro:bit and MakeCode.

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
 let Timer = 0
 input.onGesture(Gesture.Shake, function () {
     Timer = 20
+    basic.showIcon(IconNames.Happy)
+    basic.pause(5000)
+    for (let index = 0; index < 21; index++) {
+        basic.showNumber(Timer)
+        basic.pause(500)
+        Timer += -1
+    }
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.Yes)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
